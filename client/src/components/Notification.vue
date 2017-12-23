e1e7be139b7c9c2cf678b62dc3d24b63108f6d3d
Austin Gil <austin@stegosource.com>
2017-12-22 17:58:30 -0800

    Hiding notification

--- client/src/components/Notification.vue
+++ client/src/components/Notification.vue
@@ -1,35 +1,66 @@
 <template>
-  <transition name="fade">
     <div v-if="notifications" class="notifications">
-      <v-toolbar v-for="(notification, index) in notifications" :key="index" color="indigo" dark [class]="`notification notification--${notifications.type}`">
+      <v-toolbar v-for="(notification, index) in notifications" :key="index" color="indigo" dark :class="`notification notification--${notification.type}`">
         <v-toolbar-title>{{ notification.content }}</v-toolbar-title>
       </v-toolbar>
     </div>
-  </transition>
 </template>
 
-<script lang="ts">
-import Vue from "vue";
-import Component from "vue-class-component";
+<script>
+export default {
+  data() {
+    return {
+      type: null,
+      notifications: []
+    };
+  },
 
-@Component({})
-export default class Notification extends Vue {
-  type: any;
-  notifications: any[] = [
-    { content: "First notification", type: "success" },
-    { content: "Another notification", type: "error" }
-  ];
+  methods: {
+    addNotification(notification) {
+      this.notifications = [...this.notifications, notification];
+    }
+  },
 
   watch: {
-    content: () => {};
-  };
+    notifications(notifications) {
+      setTimeout(() => {
+        notifications.pop();
+      }, 5 * 1000);
+    }
+  },
 
   created() {
-    // setTimeout(() => {
-    //   this.content = "";
-    // }, 5 * 1000);
+    setTimeout(() => {
+      this.addNotification({ content: "First notification", type: "success" });
+    }, 5 * 1000);
   }
-}
+};
+
+// <script lang="ts">
+// import Vue from "vue";
+// import Component from "vue-class-component";
+
+// @Component({})
+// export default class Notification extends Vue {
+//   type: any;
+//   notifications: any[] = [];
+
+//   watch: {
+//     notifications: function =() => {
+
+//     };
+//   };
+
+//   created() {
+//     setTimeout(() => {
+//       this.addNotification({ content: "First notification", type: "success" });
+//     }, 5 * 1000);
+//   }
+
+//   addNotification(notification: any) {
+//     this.notifications = [...this.notifications, notification];
+//   }
+// }
 </script>
 
 <style scoped>
@@ -38,14 +69,5 @@ export default class Notification extends Vue {
   z-index: 50;
   bottom: 0;
   width: 100%;
-}
-
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.5s;
-}
-.fade-enter,
-.fade-leave-to {
-  opacity: 0;
 }
 </style>
