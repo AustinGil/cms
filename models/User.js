10f75e268fdd5c3ffcd80de3092a723bc3231bbb
Austin Gil <austin@stegosource.com>
2018-01-27 10:28:48 -0800

    Adding user models and seed functionality

--- models/User.js
+++ models/User.js
@@ -1,7 +1,11 @@
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define("User", {
-    username: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      unique: true
+    },
     password: DataTypes.STRING,
+    username: DataTypes.STRING,
     firstName: DataTypes.STRING,
     lastName: DataTypes.STRING,
     role: DataTypes.STRING
--- models/index.js
+++ models/index.js
@@ -1,34 +1,35 @@
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
 
-const db = {}
+const db = {};
 
 const sequelize = new Sequelize(
-  process.env['MYSQL_ENV_MYSQL_DATABASE'],
-  process.env['MYSQL_ENV_MYSQL_USER'],
-  process.env['MYSQL_ENV_MYSQL_PASSWORD'],
+  process.env["MYSQL_ENV_MYSQL_DATABASE"],
+  process.env["MYSQL_ENV_MYSQL_USER"],
+  process.env["MYSQL_ENV_MYSQL_PASSWORD"],
   {
-    dialect: process.env.DIALECT || 'mysql',
-    host: process.env['MYSQL_PORT_3306_TCP_ADDR'],
+    dialect: process.env.DIALECT || "mysql",
+    host: process.env["MYSQL_PORT_3306_TCP_ADDR"]
   }
 );
 
-fs.readdirSync(__dirname)
-  .filter(file => file !== 'index.js')
+fs
+  .readdirSync(__dirname)
+  .filter(file => file !== "index.js")
   .forEach(file => {
     const model = sequelize.import(path.join(__dirname, file));
     db[model.name] = model;
   });
 
 // Checks if any model has an association method (such as bookmarks) and fires it.
-Object.keys(db).forEach(function (modelName) {
-  if ('associate' in db[modelName]) {
-    db[modelName].associate(db)
+Object.keys(db).forEach(function(modelName) {
+  if ("associate" in db[modelName]) {
+    db[modelName].associate(db);
   }
-})
+});
 
-db.sequelize = sequelize
-db.Sequelize = Sequelize
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 
-module.exports = db
+module.exports = db;
